164221261254229832d6d275c8bccadf0affdcae
Ilia <ilia.trofimov2001@icloud.com>
2022-04-08 10:44:01 +0300

    add transitions output

--- solution_maker.py
+++ solution_maker.py
@@ -48,18 +48,21 @@ class Solution:
     def __input_data(self):
         self.__pdf.set_font('Courier', "B", size=13)
         self.__pdf.cell(0, 5, "Given automatons:", ln=1)
-        rand = random.randint(1000, 20000)
-        self.g1.save(f"g1-{rand}.dot", f"g1-{rand}.png", clear=True)
-        self.g2.save(f"g2-{rand}.dot", f"g2-{rand}.png", clear=True)
         self.__pdf.set_font('Courier', size=12)
+        rand = random.randint(1000, 20000)
+
+        self.g1.save(f"g1-{rand}.dot", f"g1-{rand}.png", clear=True)
         self.__pdf.cell(0, 5, "Automaton L1:", ln=1)
         self.__pdf.image(f"g1-{rand}.png", w=100)
-        self.__pdf.cell(0, 5, "Automaton L2:", ln=1)
-        self.__pdf.image(f"g2-{rand}.png", w=100)
-        self.__pdf.ln(10)
-
         self.__temp_files.append(f"g1-{rand}.png")
-        self.__temp_files.append(f"g2-{rand}.png")
+
+        if self.g2:
+            self.g2.save(f"g2-{rand}.dot", f"g2-{rand}.png", clear=True)
+            self.__pdf.cell(0, 5, "Automaton L2:", ln=1)
+            self.__pdf.image(f"g2-{rand}.png", w=100)
+            self.__temp_files.append(f"g2-{rand}.png")
+
+        self.__pdf.ln(10)
 
     def __print_states(self, g: DFA, title_states: str, title_terminals: str,):
         self.__pdf.cell(0, 5, title_states, ln=1)
@@ -68,7 +71,20 @@ class Solution:
         self.__pdf.cell(0, 5, title_terminals, ln=1)
         self.__pdf.cell(0, 5, ', '.join(f"<{l}>" for l in g.labels[np.where(g.terminals)[0]]), ln=1)
 
+    def __print_transitions(self, g: DFA, title: str = "Transitions are:"):
+        self.__pdf.cell(0, 5, title, ln=1)
+        for i in range(g.states_count()):
+            lbl = g.labels[i]
+            self.__pdf.cell(0, 5, f"({lbl}):", ln=1)
+            for j in range(g.alphabet_len()):
+                self.__pdf.cell(0, 5, f"\t-({g.alphabet[j]})-> ({g.labels[g.edges[i, j]]})", ln=1)
+            self.__pdf.ln(1)
+
+
     def __get__solution(self):
+        if not self.g2  and self.action != "-":
+            raise AttributeError(f"Cannot apply binary operation '{self.action}' to one automaton")
+
         self.__pdf.set_font('Courier', "B", size=13)
         self.__pdf.cell(0, 5, "Solution steps:", ln=1)
         self.__pdf.set_font('Courier', size=12)
@@ -97,25 +113,24 @@ class Solution:
 
     def __get_intersection(self):
         self.g3 = self.g1.intersection(self.g2)
-
         self.__print_states(self.g3,
                             "Resulting automaton's states are:",
                             "Terminal states are:")
+        self.__print_transitions(self.g3)
 
 
     def __get_negation(self):
         self.g3 = self.g1.negation()
-
         self.__print_states(self.g3,
                             "Resulting automaton's states are the same:",
                             "Terminal states are:")
 
     def __get_union(self):
         self.g3 = self.g1.union(self.g2)
-
         self.__print_states(self.g3,
                             "Resulting automaton's states are:",
                             "Terminal states are:")
+        self.__print_transitions(self.g3)
 
 
     def __get_complement(self):
@@ -130,4 +145,6 @@ class Solution:
         self.__print_states(self.g3,
                             "Resulting automaton's states are:",
                             "Terminal states are:")
+        self.__print_transitions(self.g3)
+
 
